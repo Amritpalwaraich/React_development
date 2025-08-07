ac0a00b99aca843879876a52b7258214c277540d
Amritpalwaraich <amritpalsinghwarich@gmail.com>
2025-08-07 20:23:22 +0530

    Movies working and getting data about it

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
 import { useEffect, useState } from "react"
 import Search from "./components/Search"
+import { useDebounce } from 'react-use'
+import MovieCard from "./components/MovieCard"
+import { getTrendingMovies, updateSearchCount } from './appwrite.js'
+import Spinner from "./components/Spinner.jsx"
 
 
-  const API_BASE_URL = 'https://api.themoviedb.org/3';
-  const API_KEY =  import.meta.env.VITE_TMDB_API_KEY;
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY =  import.meta.env.VITE_TMDB_API_KEY;
 
-  const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-     accept: 'application/json',
-    Authorization: `Bearer ${API_KEY}`
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+  Authorization: `Bearer ${API_KEY}`
   }
 }
 
 const App = () => { 
+   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
   const [errorMessage, setErrorMessage] = useState('');
+  const [searchTerm, setSearchTerm] = useState(''); 
   const [movieList, setMovieList] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
 
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  // Debouncing the search term to prevent making too many Api requests 
+  // by waiting a use to response after 500ms 
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
   
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMessage('');
+
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? 
+       `${API_BASE_URL}/search/movie?query=${encodeURIComponent(encodeURI(query))}`
+      :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -39,43 +56,81 @@ const App = () => {
 
       setMovieList(data.results || []);
 
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
 
     }
     finally {
-      console.log('Fetch movies completed');
+      setIsLoading(false);
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
     }
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
+
+useEffect ( () => {
+  loadTrendingMovies();
+}, []);
 
   return (
-    <div>
+    <main>
       <div className="pattern"/>
-      <div className="wrapper">
-        <header>
-          <img src='./hero.png' alt="Hero Banner"/>
-          <h1>Find <span className="text-gradient">Movies</span>  You'll Enjoy Without the Hassle</h1>
-          <Search />
-        </header>
+        <div className="wrapper">
+          <header>
+            <img src='./hero.png' alt="Hero Banner"/>
+            <h1>Find <span className="text-gradient">Movies</span>  You'll Enjoy Without the Hassle</h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
 
-        <section className="all-movies">
-           <h2>All Movies</h2>
+          {trendingMovies.length > 0 && (
+            <section className="trending">
+              <h2>Trending Movies</h2>
 
-           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-          
-        </section>
-      </div>
+              <ul>
+                {trendingMovies.map((movie, index) => (
+                  <li key={movie.$id}>
+                    <p>{index + 1}</p>
+                    <img src={movie.poster_url} alt={movie.title} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          )}
 
+          <section className="all-movies">
+            <h2>All Movies</h2>
 
-
-    </div>
-  )
-  
-}
+            
+            {isLoading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </ul>
+            )}
+          </section>
+    </ div>
+    </main>
+    )
+  }
 
 export default App
